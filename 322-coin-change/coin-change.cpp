fb01015c019c9dc9a2973beb0d17f517b11e7e39
mahak <mahaklachhwani0412@gmail.com>
2025-09-23 00:20:42 +0530

    Time: 11 ms (98.11%) | Memory: 18 MB (73.71%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, 1e9);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == 1e9 ? -1 : dp[amount];
+    }
+};
