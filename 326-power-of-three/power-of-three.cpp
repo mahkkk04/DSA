b0683ee99ed3d9ac9a092131aeb8f3d53482143a
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-02 01:15:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (16.55%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return n > 0 && 1162261467 % n == 0;
+        if (n <= 0) return false;
+        if (n == 1) return true;      
+        if (n % 3 != 0) return false; 
+        return isPowerOfThree(n / 3); 
     }
 };
