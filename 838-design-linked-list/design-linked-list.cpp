7ed608f79bc5c61d6754adb35dbfe4ce3c694984
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-03 23:43:11 +0530

    Time: 15 ms (23.63%) | Memory: 26 MB (35.96%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.cpp
@@ -0,0 +1,50 @@
+class MyLinkedList {
+    struct Node {
+        int val;
+        Node* next;
+        Node(int v): val(v), next(nullptr) {}
+    };
+    Node* head;
+    int size;
+
+public:
+    MyLinkedList() {
+        head = new Node(0); 
+        size = 0;
+    }
+    
+    int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node* cur = head->next;
+        for (int i = 0; i < index; i++) cur = cur->next;
+        return cur->val;
+    }
+    
+    void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+    
+    void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+    
+    void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+        Node* prev = head;
+        for (int i = 0; i < index; i++) prev = prev->next;
+        Node* node = new Node(val);
+        node->next = prev->next;
+        prev->next = node;
+        size++;
+    }
+    
+    void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        Node* prev = head;
+        for (int i = 0; i < index; i++) prev = prev->next;
+        Node* toDelete = prev->next;
+        prev->next = toDelete->next;
+        delete toDelete;
+        size--;
+    }
+};
