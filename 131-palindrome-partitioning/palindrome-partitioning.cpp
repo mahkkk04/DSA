f6461bf9fc1efe5712dea74115cc40582fd52695
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-05 00:28:02 +0530

    Time: 29 ms (92.06%) | Memory: 53.5 MB (76.04%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -4,33 +4,33 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
+        vector<vector<string>> res;
         vector<string> path;
-        dfs(s, 0, path, result);
-        return result;
+        backtrack(s, 0, path, res);
+        return res;
     }
 
 private:
-    bool isPalindrome(string s, int start, int end) {
-        while (start < end) {
-            if (s[start] != s[end]) return false;
-            start++;
-            end--;
+    bool isPalindrome(const string &s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
         }
         return true;
     }
 
-    void dfs(string &s, int start, vector<string> &path, vector<vector<string>> &result) {
+    void backtrack(const string &s, int start, vector<string> &path, vector<vector<string>> &res) {
         if (start == s.size()) {
-            result.push_back(path);
+            res.push_back(path);
             return;
         }
 
         for (int end = start; end < s.size(); end++) {
             if (isPalindrome(s, start, end)) {
                 path.push_back(s.substr(start, end - start + 1));
-                dfs(s, end + 1, path, result);
-                path.pop_back();
+                backtrack(s, end + 1, path, res);
+                path.pop_back(); 
             }
         }
     }
