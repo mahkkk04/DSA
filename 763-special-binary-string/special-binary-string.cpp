b619cc7bf5010055107446a07a366f64ee3c1630
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-08 00:30:58 +0530

    Time: 3 ms (20.48%) | Memory: 9.4 MB (62.10%) - LeetSync

--- 763-special-binary-string/special-binary-string.cpp
+++ 763-special-binary-string/special-binary-string.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     string makeLargestSpecial(string s) {
+
         vector<string> subs;
         int count=0,start=0;
         for (int i=0;i<s.size();i++) {
-            if (s[i]=='1') count++;
+            if (s[i]=='1') 
+                count++;
             else count--;
             if (count==0) {
                 string inner = s.substr(start+1,i-start-1);
@@ -12,6 +14,7 @@ public:
                 start=i+1;
             }
         }
+
         sort(subs.begin(),subs.end(),greater<string>());
         string res;
         for (auto &x:subs) res+=x;
