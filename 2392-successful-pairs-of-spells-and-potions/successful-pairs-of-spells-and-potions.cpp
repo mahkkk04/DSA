142991d86ce34e22e89a5f2ec798cf6f342a90bd
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-09 00:38:49 +0530

    Time: 51 ms (22.04%) | Memory: 128.1 MB (53.93%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,28 +1,18 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        sort(potions.begin(), potions.end());
-        int n = spells.size();
-        int m = potions.size();
-        vector<int> result(n);
-
-        for (int i = 0; i < n; ++i) {
-            long long target = (success + spells[i] - 1) / spells[i];
-            int left = 0, right = m - 1, idx = m;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (potions[mid] >= target) {
-                    idx = mid;
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-
-            result[i] = m - idx;
+    std::vector<int> successfulPairs(std::vector<int>& spells, std::vector<int>& potions, long long success) {
+        std::vector<int> result;
+        int potionCount = potions.size();
+        std::sort(potions.begin(), potions.end());
+        for (int spellStrength : spells) {
+            long long minPotionStrength = (success + spellStrength - 1) / spellStrength;
+            int firstValidIndex = std::lower_bound(potions.begin(), potions.end(), minPotionStrength) - potions.begin();
+            result.push_back(potionCount - firstValidIndex);
         }
-
+        
         return result;
     }
 };
