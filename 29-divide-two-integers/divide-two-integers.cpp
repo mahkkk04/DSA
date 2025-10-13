488835da1d4d2e95d355294e8351723d31469fb4
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-14 00:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.14%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
 
+        //for handling overflow as the negative of intmin will exceed the rnge 
+        
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        
+        //for deciding if neg or not
         bool negative = (dividend < 0) != (divisor < 0);
+
+        //for getting sign free numbs
         long long ldividend = abs((long long)dividend);
         long long ldivisor = abs((long long)divisor);
         long long quotient = 0;
+        
 
+        //bit shifts 
         while (ldividend >= ldivisor) {
             long long sum = ldivisor, multiple = 1;
-
             while (ldividend >= (sum + sum)) {
                 sum += sum;
                 multiple += multiple;
             }
-
             ldividend -= sum;
             quotient += multiple;
         }
