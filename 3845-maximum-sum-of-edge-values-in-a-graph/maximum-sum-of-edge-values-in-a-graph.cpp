499f28a69b6471a280993863000105616cba58f5
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-16 15:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 250.6 MB (73.55%) - LeetSync

--- 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
+++ 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     long long maxScore(int n, vector<vector<int>>& edges) {
-       
+
+    //if there is only one node then return 0 
+        if (n == 1){
+            return 0LL;  //for handling overflow
+        }
+        
+    //initializing the max score to 0
         long long ans = 0;
+
+    //multiplying the two largest nums so as to get max result
         ans += 1LL * n * (n - 1);
+
+    //1 4 6 5 3 2 and 1 3 6 5 4 2 and 1 4 5 6 3 2 and ----1 3 5 6 4 2---- possible combinations for test case n=6 , dash wala is the best possible version of arrangement for a mathematical condition i*(i-2) 
         for (int i = n; i >= 3; i--){
             ans += 1LL * i * (i - 2);
         }
-
+    
+    //for handling the cycles case : first and last node will be assinged the smallest value 1 and 2 only
         if (edges.size() == n){
             ans += 2;
         }
