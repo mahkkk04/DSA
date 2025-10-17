9461b1eb6740ae2563156dc1d75e3f8fc3632abe
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-17 22:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (93.80%) - LeetSync

---
+++ 400-nth-digit/nth-digit.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findNthDigit(int n) {
+        long long len = 1, count = 9, start = 1;
+        while (n > len * count) {
+            n -= len * count;
+            len++;
+            count *= 10;
+            start *= 10;
+        }
+        long long num = start + (n - 1) / len;
+        string s = to_string(num);
+        return s[(n - 1) % len] - '0';
+    }
+};
