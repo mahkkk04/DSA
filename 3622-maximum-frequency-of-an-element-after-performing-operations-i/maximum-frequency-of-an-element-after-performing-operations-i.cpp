ceea2d9456dbed87b25462f882857268e083d9fb
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-24 17:27:32 +0530

    Time: 494 ms (12.93%) | Memory: 172.3 MB (14.79%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         sort(nums.begin(), nums.end());
-
         int ans = 0;
         unordered_map<int, int> numCount;
+        set<int> modes;
+
+        auto addMode = [&](int value) {
+            modes.insert(value);
+            if (value - k >= nums.front()) {
+                modes.insert(value - k);
+            }
+            if (value + k <= nums.back()) {
+                modes.insert(value + k);
+            }
+        };
 
         int lastNumIndex = 0;
         for (int i = 0; i < nums.size(); ++i) {
             if (nums[i] != nums[lastNumIndex]) {
                 numCount[nums[lastNumIndex]] = i - lastNumIndex;
                 ans = max(ans, i - lastNumIndex);
+                addMode(nums[lastNumIndex]);
                 lastNumIndex = i;
             }
         }
 
         numCount[nums[lastNumIndex]] = nums.size() - lastNumIndex;
         ans = max(ans, (int)nums.size() - lastNumIndex);
+        addMode(nums[lastNumIndex]);
 
         auto leftBound = [&](int value) {
             int left = 0, right = nums.size() - 1;
@@ -44,13 +56,13 @@ public:
             return left;
         };
 
-        for (int i = nums.front(); i <= nums.back(); i++) {
-            int l = leftBound(i - k);
-            int r = rightBound(i + k);
+        for (int mode : modes) {
+            int l = leftBound(mode - k);
+            int r = rightBound(mode + k);
 
             int tempAns;
-            if (numCount.count(i)) {
-                tempAns = min(r - l + 1, numCount[i] + numOperations);
+            if (numCount.count(mode)) {
+                tempAns = min(r - l + 1, numCount[mode] + numOperations);
             } else {
                 tempAns = min(r - l + 1, numOperations);
             }
