cca587aa897dde7212c02d1f0631703545cc61f3
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-27 16:17:47 +0530

    Time: 229 ms (10.05%) | Memory: 147.9 MB (8.80%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
+        // Sort the nums array
         sort(nums.begin(), nums.end());
-        int cnt = 0, prev = INT_MIN;
+        
+        // Set to store distinct elements
+        unordered_set<int> distinct;
+        
+        // The next smallest value that can be used as a distinct element
+        int nextAvailable = nums[0] - k;
+
+        // Iterate over the sorted array
         for (int num : nums) {
-            int curr = min(max(num - k, prev + 1), num + k);
-            if (curr > prev) {
-                cnt++;
-                prev = curr;
+            // Ensure the next available value is within the range [num - k, num + k]
+            if (nextAvailable < num - k) {
+                nextAvailable = num - k;
+            }
+            
+            // If nextAvailable is within the range [num - k, num + k], add it to the set
+            if (nextAvailable <= num + k) {
+                distinct.insert(nextAvailable);
+                nextAvailable++; // Move to the next available distinct value
             }
         }
-        return cnt;
+        
+        // Return the size of the distinct set
+        return distinct.size();
     }
 };
