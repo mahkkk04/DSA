782a9a0ea497d03504eee8858d7ce85d921b2bde
Mahak Lachhwani <mahaklachhwani0412@gmail.com>
2025-10-29 15:53:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (30.21%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long smallestNumber(long long n) {
         if (n <= 0) return 0;
-        int msb = 63 - __builtin_clzll((unsigned long long)n); // position of highest set bit
-        return ( (1ULL << (msb + 1)) - 1 ); // 2^(msb+1) - 1
+        int msb = 63 - __builtin_clzll((unsigned long long)n);
+        return ( (1ULL << (msb + 1)) - 1 );
     }
 };
