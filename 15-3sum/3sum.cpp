4135a2436fdbabaabc78b29e859104f0d5e918bf
Mahak <mahaklachhwani0412@gmail.com>
2025-12-29 23:42:50 +0530

    Time: 39 ms (93.63%) | Memory: 28.9 MB (98.86%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,30 +1,33 @@
+//first sorting
+//then twosum using twopointer
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
         int n=nums.size();
-        vector<vector<int>> ans;
-        for(int i=0;i<n-2;i++)
-        { 
-            if(i>0 && nums[i]==nums[i-1]) continue;
-           int j=i+1;
-           int k=n-1;
-           while(j<k)
-           {
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum<0)
-            j++;
-            else if(sum>0)
-            k--;
-            else{
-               vector<int> temp={nums[i],nums[j],nums[k]};
-               ans.push_back(temp);
-               j++;
-               k--;
-               while(j<k && nums[j]==nums[j-1]) j++;
-               while(j<k && nums[k]==nums[k+1]) k--; }
-        }
-    }
-        return ans;    
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;++i){
+            if(i>0&&nums[i]==nums[i-1])continue;
+                int target=-nums[i];
+                int left=i+1,right=n-1;
+                while(left<right){
+                int sum=nums[left]+nums[right];
+                if(sum==target){
+                    result.push_back({nums[i], nums[left], nums[right]});
+                
+                while(left<right && nums[left]==nums[left+1])left++;
+                while(left<right && nums[right]==nums[right-1])right--;
+                left++;right--;
+                }
+                
+                else if(sum<target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }   
+        return result;
     }
 };
