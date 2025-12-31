f99bc6c4ded1c0807441d59b030fef33ea8b5274
Mahak <mahaklachhwani0412@gmail.com>
2025-12-31 18:11:55 +0530

    Time: 16 ms (7.86%) | Memory: 15.1 MB (5.95%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_map<ListNode*,bool>visited;
+        while(head!=nullptr){
+            if(visited.find(head)!=visited.end())
+            return true;
+            visited[head]=true;
+            head=head->next;
+
+        }
+        return false;
+    }
+};
+
+//optimized version
+//Floyd’s Cycle Detection (Tortoise & Hare)
