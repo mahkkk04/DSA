f04f8450c06e030a412a9d633917cfa664f6c334
Mahak <mahaklachhwani0412@gmail.com>
2026-01-01 22:13:51 +0530

    Time: 11 ms (42.76%) | Memory: 14.2 MB (65.04%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                if (!st.empty()) {
+                    st.pop();
+                } else {
+                    s[i] = '*';
+                }
+            }
+        }
+        while (!st.empty()) {
+            s[st.top()] = '*';
+            st.pop();
+        }
+        string ans;
+        for (char c : s) {
+            if (c != '*') ans += c;
+        }
+        return ans;
+    }
+};
