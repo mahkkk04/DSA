73dd76ccfedad0c7d658e65c4a74b3b0fc01110e
Mahak <mahaklachhwani0412@gmail.com>
2026-01-04 17:35:58 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (24.29%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>candies(n,1);
+        for(int i=0;i<n-1;++i){
+            if(ratings[i]<ratings[i+1]){
+                candies[i+1]=candies[i]+1;
+            }
+        }
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1]>ratings[i]){
+                candies[i-1]=max(candies[i-1],candies[i]+1);
+            }
+        }
+        int ans=0;
+        for(auto c:candies){
+            ans=ans+c;
+        }
+        return ans;
+    }
+};
