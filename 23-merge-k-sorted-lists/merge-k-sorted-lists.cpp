3537c17b5caaef84b8ca3a8149fc7db919ed8f57
Mahak <mahaklachhwani0412@gmail.com>
2026-01-23 19:32:06 +0530

    Time: 9 ms (24.29%) | Memory: 18.4 MB (66.00%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,26 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        auto cmp = [](ListNode* a, ListNode* b){ return a->val > b->val; };
-        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
-        for (auto l : lists) if (l) pq.push(l);
-        ListNode dummy(0), *cur = &dummy;
-        while (!pq.empty()) {
-            auto node = pq.top(); pq.pop();
-            cur->next = node;
-            cur = cur->next;
-            if (node->next) pq.push(node->next);
+
+priority_queue<ListNode*, vector<ListNode*>,function<bool(ListNode*, ListNode*)>> pq([](ListNode* a, ListNode* b) {
+            return a->val > b->val; // min heap
+        });
+
+        // Push first node of each list
+        for (auto list : lists) {
+            if (list != nullptr) {
+                pq.push(list);
+            }
         }
-        return dummy.next;
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        // Process heap
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+
+            tail->next = node;
+            tail = tail->next;
+
+            if (node->next != nullptr) {
+                pq.push(node->next);
+            }
+        }
+
+        return dummy->next;
     }
 };
